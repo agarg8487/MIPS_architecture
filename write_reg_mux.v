f552213c76732915b51f75e01f85807153532b85
vijaysuryav <vsv10@iitbbs.ac.in>
2019-09-10 19:54:57 +0530

    changes by vijay at 8:00 pm

--- write_reg_mux.v
+++ write_reg_mux.v
@@ -3,8 +3,4 @@ module mod_write_reg_mux(input [4:0] ins_20_16,
                      input reg_dst,
                      output wire [4:0] write_reg_add);
 assign write_reg_add=(reg_dst) ? ins_20_16:ins_15_11;
-<<<<<<< HEAD
-endmodule    
-=======
 endmodule
->>>>>>> refs/remotes/origin/master
