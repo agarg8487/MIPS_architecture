72c1b5c2fcc2612d5692df7d94902cf90aa1e0e1
sunaditya <at14@iitbbs.ac.in>
2019-10-23 21:17:08 +0530

    added code for instructions to binary

--- simulators/code_to_bin.py
+++ simulators/code_to_bin.py
@@ -10,3 +10,38 @@ import reg_bin as rgb
 import opcode_bin as opb
 import immd_bin as imb
 #---------------------------------------------------------------------------
+def code_to_bin (instruction_list,file_name) :    
+    i = 0
+    file_name = (file_name + '.txt')
+    file_handle = open(file_name,'w+')
+    while ( i  < len(instruction_list)) :
+        opcode = opb.opcode_bin(instruction_list[i])
+        opcode_bin = opcode[0]
+        funct_bin = opcode[1]
+        op_type = opcode[2]
+        if (op_type == 'r') :
+            rs_bin = rgb.reg_bin(instruction_list[i + 1])
+            rt_bin = rgb.reg_bin(instruction_list[i + 2])
+            rd_bin = rgb.reg_bin(instruction_list[i + 3])
+            instruction_bin = (opcode_bin + rs_bin + rt_bin + rd_bin + '00000' + funct_bin)
+            i = i + 4
+            file_handle.write(instruction_bin)
+            file_handle.write('\n')
+        if (op_type == 'i') :
+            rs_bin = rgb.reg_bin(instruction_list[i + 1])
+            rt_bin = rgb.reg_bin(instruction_list[i + 2])
+            immd_bin = imb.immd_bin15(instruction_list[i + 3])
+            instruction_bin = (opcode_bin + rs_bin + rt_bin + immd_bin)
+            i = i + 4
+            file_handle.write(instruction_bin)
+            file_handle.write('\n')
+        if (op_type == 'j') :
+            immd_bin = imb.immd_bin26(instruction_list[i + 1])
+            instruction_bin = (opcode_bin + immd_bin)
+            i = i + 2
+            file_handle.write(instruction_bin)
+            file_handle.write('\n')
+
+
+
+
--- simulators/processor_tester.py
+++ simulators/processor_tester.py
@@ -6,13 +6,13 @@ import os
 import subprocess
 #--------------------------------------------------------------------------------------
 #-------------------------------project imports----------------------------------------
-#import code_to_bin as ctb
+import code_to_bin as ctb
 #--------------------------------------------------------------------------------------
 
 #----------------check file existence, convert to binary--------------------------------
 os.chdir('../assembly_codes')
-print('printing all the files in the assembly_codes directory')
-print(os.path.exists('trial.vsat'))
+#print('printing all the files in the assembly_codes directory')
+#print(os.path.exists('trial.vsat'))
 file_list = os.listdir()
 for file_name in file_list :
     print(file_name)
@@ -22,15 +22,16 @@ while (not(os.path.exists(file_req))) :
     print('non existing file name, try again or type exit')
     file_req = input('\nenter your file name : ')
     if(file_req == 'exit') : exit()
-
+file_name_split = file_req.split('.')
+file_name = file_name_split[0]
 instruction_list = list()
 with open(file_req,'r') as ins_file :
     for line in ins_file :
-        for word in line.split():
+        for word in line.split() :
             instruction_list.append(word)
-            print(word) # testing the code
+#            print(word) # testing the code
 
-#ctb.code_to_bin(instruction_list)
+ctb.code_to_bin(instruction_list,file_name)
 #-------------------------------------------------------------------------------------
 
 #-----------------------------compile and run the test bench--------------------------
